bfcedb1505ba286189c16642ee751c43e4c161b1
Widya Tifanny Dewi <37367015+widwood@users.noreply.github.com>
2019-03-25 11:35:08 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$(".portfolio-single-slider").slick({
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 2000
+
+	});
+
+	$(".location-slider").slick({
+		slidesToShow:1,
+		infinite: true,
+		arrows:false,
+		autoplay: true,
+  		autoplaySpeed: 3000
+	});
+
+});
+
+
+
+
